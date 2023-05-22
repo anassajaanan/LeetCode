c39f14d0f5cca8405eb1d35b59a9439a2edee292
csstudentanas <p131047727@taalim.ma>
2023-05-22 12:09:33 +0100

    N-Ary Tree Preorder traversal

--- N-ary Tree Preorder Traversal/main.c
+++ N-ary Tree Preorder Traversal/main.c
@@ -7,26 +7,67 @@ struct Node {
     struct Node** children;
 };
 
-void	getPreorder(struct Node *p, int *ans, int *index)
+typedef struct node
 {
-	if (p)
-	{
-		ans[(*index)++] = p->val;
-		for (int i = 0; i < p->numChildren; i++)
-		{
-			getPreorder(p->children[i], ans, index);
-		}
-	}
+    void *data;
+    struct node *next;
+} node;
+
+typedef struct stack
+{
+    node *top;
+} stack;
+
+void createStack(stack *s)
+{
+    s->top = NULL;
+}
+
+void Push(stack *s, void *data)
+{
+    node *new_node;
+
+    new_node = (node *)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = s->top;
+    s->top = new_node;
+}
+
+void *Pop(stack *s)
+{
+    node *stack_top;
+    void *x;
+
+    stack_top = s->top;
+    s->top = s->top->next;
+    x = stack_top->data;
+    free(stack_top);
+    return x;
 }
 
 int* preorder(struct Node* root, int* returnSize) {
-	int index;
-    int *ans;
+	stack		s;
+	struct Node	*p;
+	int			i;
+	int			*ans;
 
-	index = 0;
+	if (root == NULL)
+	{
+		*returnSize = 0;
+		return (NULL);
+	}
+	createStack(&s);
+	Push(&s, root);
+	i = 0;
 	ans = (int *)malloc(sizeof(int) * 10000);
-	getPreorder(root, ans, &index);
-	*returnSize = index;
+	while (s.top != NULL)
+	{
+		p = (struct Node *)Pop(&s);
+		ans[i++] = p->val;
+		for (int j = p->numChildren - 1; j >= 0; j--)
+			Push(&s, p->children[j]);
+	}
+	*returnSize = i;
 	return (ans);
 }
 
