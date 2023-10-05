21e0a6c9c90712a393a7fe79b94ec67feb4a9724
Anas Ajaanan <aajaanan@lab1r3s13.42abudhabi.ae>
2023-10-05 16:56:40 +0400

    Sqrt using Binary Search Algorithm

---
+++ Sqrt(x)/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int	mySqrt(int x)
+{
+	long	start, mid, end;
+
+	if (x == 0 || x == 1)
+		return (x);
+	start = 0;
+	end = x / 2;
+	while (start < end)
+	{
+		mid = (start + end) / 2;
+		if (mid == start)
+			return (end * end <= x ? end : mid);
+		if (mid * mid < x)
+			start = mid;
+		else if (mid * mid > x)
+			end = mid;
+		else
+			return (mid);
+	}
+	return (mid);
+}
+
+
+int main(void)
+{
+
+	printf("The square root is : %i\n", mySqrt(2147395599));
+}
